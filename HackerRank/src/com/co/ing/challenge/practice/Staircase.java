cbc2e6c22881bfacde8c5f0103f98a26cde9367a
Edwin <ed.cabezas7@gmail.com>
2018-10-29 22:18:33 -0500

    staircase resuelto

---
+++ HackerRank/src/com/co/ing/challenge/practice/Staircase.java
@@ -0,0 +1,38 @@
+package com.co.ing.challenge.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Edwin Cabezas
+ *
+ */
+public class Staircase {
+	
+	
+    static void staircase(int n) {
+    	String stairString = new String();
+    	
+    	for(int i = 1; i <= n; i++) {
+    		stairString = String.join(stairString, Collections.nCopies(n - i, " "))
+    				.concat(String.join(stairString, Collections.nCopies(i, "#") ));
+    		System.out.println(stairString);
+    		stairString = new String();
+    	}
+    }
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+
+}
